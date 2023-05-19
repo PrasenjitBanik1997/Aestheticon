7c30f1b856d82c19835da4e9e126c2ff79fa15ab
PrasenjitBanik1997 <prasenjitbanik576@gmail.com>
2023-05-19 18:21:39 +0530

    changes1

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,54 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Swipedrawer from '../drawer/Swipedrawer';
 import { material } from '../../library/material';
 import { useNavigate } from 'react-router-dom';
-import { getClinicForInjector } from '../../services/ClinicService';
 import { connect } from 'react-redux';
 import { getClinicAction } from '../../store/action/Action';
-import { styled } from '@mui/material/styles';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 
 
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-    [`&.${tableCellClasses.head}`]: {
-        backgroundColor: "#80d8ff",
-        color: theme.palette.common.white,
-        fontWeight: "bold",
-    },
-    [`&.${tableCellClasses.body}`]: {
-        fontSize: 14,
-    },
-}));
 
 
-let clinicDetalis;
-let clinicId;
+
 
 function Dashboard(props) {
-    const { userData, getClinicDetails } = props;
+    const { userData } = props;
     let userDetails = userData.authReducer.data;
     const navigate = useNavigate();
-    const [hideAndShow, setHideAndShow] = useState(true);
-    const [clinicName, setClinicName] = useState([])
 
-    useEffect(() => {
-        // if (userDetails.role === "INJECTOR") {
-        //     clinicDetailsForInjector()
-        // }
-    }, []);
 
-    // const clinicDetailsForInjector = async () => {
-    //     await getClinicForInjector()
-    //         .then((resp) => {
-    //             clinicDetalis = resp.data;
-    //             let clinicName = clinicDetalis.map((ele) => ele.clinicName);
-    //             setClinicName(clinicName)
-    //         })
-    //         .catch((error) => {
-
-    //         })
-    // };
 
     const handleClick = (value) => {
         if (value.action === "patientManagement") {
@@ -64,21 +31,11 @@ function Dashboard(props) {
         }
     };
 
-    // const selectClinic = (e, value) => {
-    //     let clinicData = clinicDetalis.filter((ele) => ele.clinicName === value);
-    //     clinicId = clinicData.map((resp) => resp.clinicId)
-    //     getClinicDetails(...clinicData);
-    //     navigate("/dashboard/patient-list");
-    // };
-
 
     return (
         <div className='body'>
             <Swipedrawer />
             <div className='row'>
-                {/* <div className='col-12'>
-                    <ClinicList />
-                </div> */}
                 <div className='col-lg-3 col-md-6 col-sm-12 mt-3' hidden={userDetails.role === "PRESCRIBER"}>
                     <material.Card sx={{ backgroundColor: "lightblue" }}>
                         <material.CardActionArea onClick={() => handleClick({ "action": "patientManagement" })} sx={{ pt: 3, pb: 3 }}>
@@ -87,16 +44,6 @@ function Dashboard(props) {
                             </material.CardContent>
                         </material.CardActionArea>
                     </material.Card>
-                    {/* <div hidden={hideAndShow}>
-                        <material.Autocomplete
-                            id="orgId"
-                            className='mt-3'
-                            onChange={selectClinic}
-                            options={clinicName}
-                            renderInput={(params) => <material.TextField {...params} variant="standard" label="Select Clinic Name"
-                            />}
-                        />
-                    </div> */}
                 </div>
                 {userDetails.role === "MANAGER" ? (
                     <div className='col-lg-3 col-md-6 col-sm-12 mt-3'>
